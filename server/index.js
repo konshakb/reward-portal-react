84631b2d180e661913d1ab45f47c5ddd3e46dac8
bryankle <bryan@bryanle.io>
2019-01-29 15:53:04 -0500

    Refactored server index

--- server/index.js
+++ server/index.js
@@ -6,8 +6,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mysql = require('./database/dbcon.js');
-// const getUsers = require('./database/queries');
-const User = require('./database/queries/user');
+
 app.use(morgan('combined'));
 app.use(bodyParser.json({ type: '*/*' }));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,29 +15,6 @@ app.use(cors());
 const port = process.env.PORT || 3001;
 
 router(app);
-// app.get('/api/test', function(req, res) {
-//     console.log('Established connection to /api/test');
-//     res.send('Hello');
-// })
-
-// const getUserByEmail = function(email) {
-//     return new Promise(function(resolve, reject) {
-//         const params = [email];
-//         mysql.pool.query('SELECT password FROM user WHERE email = ?', params,
-//         function(err, data) {
-//             if (err) reject(err);
-//             resolve(data);
-//         })
-//     })
-// }
-
-// app.get('/test', function(req, res) {
-//     getUserByEmail('bryan@bryan.com')
-//     .then(function(user) {
-//         res.send(user);
-//     })
-// })
-
 
 app.listen(port, function() {
     console.log('Listening on port: ', port);
