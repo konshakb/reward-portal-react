af6f81839e021d5a69b2552bd27b2fb90625c9c2
Bryan Konshak <bkonshak@gmail.com>
2019-02-22 21:19:23 -0500

    removing node modules and adding to ignore

--- client/src/components/SemanticForm.js
+++ client/src/components/SemanticForm.js
@@ -1,15 +1,41 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Form, Input } from 'semantic-ui-react';
+import React from "react";
+import PropTypes from "prop-types";
+import { Form, Input } from "semantic-ui-react";
 
-export default function semanticFormField ({ input, type, label, placeholder, meta: { touched, error, warning }, as: As = Input, ...props }) {
-  function handleChange (e, { value }) {
+export default function semanticFormField({
+  input,
+  type,
+  label,
+  placeholder,
+  meta: { touched, error, warning },
+  as: As = Input,
+  ...props
+}) {
+  function handleChange(e, { value }) {
     return input.onChange(value);
   }
   return (
     <Form.Field>
-      <As {...props} {...input} value={input.value} type={type} label={label} placeholder={placeholder} onChange={handleChange} />
-      {touched && ((error && <span><i>{error}</i></span>) || (warning && <span><i>{warning}</i></span>))}
+      <As
+        {...props}
+        {...input}
+        value={input.value}
+        type={type}
+        label={label}
+        placeholder={placeholder}
+        onChange={handleChange}
+      />
+      {touched &&
+        ((error && (
+          <span>
+            <i>{error}</i>
+          </span>
+        )) ||
+          (warning && (
+            <span>
+              <i>{warning}</i>
+            </span>
+          )))}
     </Form.Field>
   );
 }
--- client/src/containers/CreateUser.js
+++ client/src/containers/CreateUser.js
@@ -1,28 +1,36 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { compose } from 'redux';
-import { reduxForm, Field } from 'redux-form';
-import { Button, Form, Grid, Header, Image, Message, Segment, Input } from 'semantic-ui-react';
-import * as actions from '../actions';
-import semanticFormField from '../components/SemanticForm';
-import requireAdmin from './requireAdmin';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { compose } from "redux";
+import { reduxForm, Field } from "redux-form";
+import {
+  Button,
+  Form,
+  Grid,
+  Header,
+  Image,
+  Message,
+  Segment,
+  Input
+} from "semantic-ui-react";
+import * as actions from "../actions";
+import semanticFormField from "../components/SemanticForm";
+import requireAdmin from "./requireAdmin";
 
 class CreateUser extends Component {
-  onSubmit = (formProps) => {
+  onSubmit = formProps => {
     console.log(formProps);
     this.props.createUser(formProps, () => {
-        // TODO: Determine page to redirect to after admin adds user
-      this.props.history.push('/');
+      // TODO: Determine page to redirect to after admin adds user
+      this.props.history.push("/");
     });
   };
-   
-  render() {
 
+  render() {
     const { handleSubmit } = this.props; // handleSubmit provided by redux form
 
     return (
-    <div className='login-form'>
-      <style>{`
+      <div className="login-form">
+        <style>{`
         body > div,
         body > div > div,
         body > div > div > div.login-form {
@@ -32,30 +40,84 @@ class CreateUser extends Component {
           padding-top: 5em
         }
       `}</style>
-      <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle'>
-        <Grid.Column style={{ maxWidth: 450 }}>
-          <Header as='h2' color='teal' textAlign='center'>
-            Create a user
-          </Header>
-          <Form size='large' onSubmit={handleSubmit(this.onSubmit)}>
-            <Segment stacked>
-              <Field name="email" component={semanticFormField} as={Form.Input} icon='mail' iconPosition='left' type="text" placeholder="Email" />
-              <Field name="password" component={semanticFormField} as={Form.Input} icon='lock' iconPosition='left' type="password" placeholder="Password" />
-              <div>{this.props.errorMessage}</div>
-              <Button color='teal' fluid size='large'>
-                Submit
-              </Button>
-            </Segment>
-          </Form>
-        </Grid.Column>
-      </Grid>
-    </div>
-  )}  
+        <Grid
+          textAlign="center"
+          style={{ height: "100%" }}
+          verticalAlign="middle"
+        >
+          <Grid.Column style={{ maxWidth: 450 }}>
+            <Header as="h2" color="teal" textAlign="center">
+              Create a user
+            </Header>
+            <Form size="large" onSubmit={handleSubmit(this.onSubmit)}>
+              <Segment stacked>
+                <Field
+                  name="first_name"
+                  component={semanticFormField}
+                  as={Form.Input}
+                  icon="lock"
+                  iconPosition="left"
+                  type="text"
+                  placeholder="First Name"
+                />
+                <Field
+                  name="last_name"
+                  component={semanticFormField}
+                  as={Form.Input}
+                  icon="lock"
+                  iconPosition="left"
+                  type="text"
+                  placeholder="Last Name"
+                />
+                <Field
+                  name="region_id"
+                  component={semanticFormField}
+                  as={Form.Input}
+                  icon="lock"
+                  iconPosition="left"
+                  type="text"
+                  placeholder="Region"
+                />
+                <Field
+                  name="email"
+                  component={semanticFormField}
+                  as={Form.Input}
+                  icon="mail"
+                  iconPosition="left"
+                  type="text"
+                  placeholder="Email"
+                />
+                <Field
+                  name="password"
+                  component={semanticFormField}
+                  as={Form.Input}
+                  icon="lock"
+                  iconPosition="left"
+                  type="password"
+                  placeholder="Password"
+                />
+                <div>{this.props.errorMessage}</div>
+                <Button color="teal" fluid size="large">
+                  Submit
+                </Button>
+              </Segment>
+            </Form>
+          </Grid.Column>
+        </Grid>
+      </div>
+    );
+  }
 }
 
 function mapStateToProps(state) {
   return { errorMessage: state.auth.errorMessage };
 }
 
-export default compose (connect(mapStateToProps, actions), reduxForm({ form: 'signup'}), requireAdmin)(CreateUser);
-
+export default compose(
+  connect(
+    mapStateToProps,
+    actions
+  ),
+  reduxForm({ form: "signup" }),
+  requireAdmin
+)(CreateUser);
