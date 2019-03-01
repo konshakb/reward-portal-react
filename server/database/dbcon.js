063ec6728bc89074295111aa6c955a074f570299
Bryan Konshak <bkonshak@gmail.com>
2019-02-28 20:25:11 -0500

    merged

--- server/database/dbcon.js
+++ server/database/dbcon.js
@@ -3,7 +3,7 @@ const pool = mysql.createConnection({
   connectionLimit: 10,
   host: "127.0.0.1",
   user: "root",
-  password: "Bk10244994!",
+  password: "password",
   database: "nihal"
 });
 module.exports.pool = pool;
